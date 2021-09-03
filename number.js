0246d336fff3f2e4c517c039bcc11e636993e72a
ShashankMuthuraju <shashankmuthuraju@gmail.com>
2021-09-03 18:56:23 -0500

    initial commit

---
+++ number.js
@@ -0,0 +1,16 @@
+console.log(10)
+for(let i=10; i<=100; i=i+1){
+    let remainder=i%3
+    if(remainder===0){
+        console.log(i)
+    }
+    
+} 
+
+for(let i=100; i>=10; i=i-1){
+    let remainder=i%3
+    if(remainder===0){
+        console.log(i)
+    }
+  
+} 
